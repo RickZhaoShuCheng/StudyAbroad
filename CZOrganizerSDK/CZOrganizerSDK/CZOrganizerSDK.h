14a42ce119056f2a205611584cc0157cffdf4e7a
赵曙诚 <shucheng.zhao@quanshi.com>
2020-02-21 14:11:50 +0800

    feat：first commit

---
+++ CZOrganizerSDK/CZOrganizerSDK/CZOrganizerSDK.h
@@ -0,0 +1,19 @@
+//
+//  CZOrganizerSDK.h
+//  CZOrganizerSDK
+//
+//  Created by zsc on 2020/2/21.
+//  Copyright © 2020 zsc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CZOrganizerSDK.
+FOUNDATION_EXPORT double CZOrganizerSDKVersionNumber;
+
+//! Project version string for CZOrganizerSDK.
+FOUNDATION_EXPORT const unsigned char CZOrganizerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CZOrganizerSDK/PublicHeader.h>
+
+
