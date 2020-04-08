fac25ce3b14ec059e9eb8d827200d4f655900326
赵曙诚 <superchengyi@163.com>
2020-04-09 00:13:10 +0800

    feat:增加精华日志和优质评论接口

---
+++ CZOrganizerSDK/CZOrganizerSDK/UI/Search/CZSearchAllViewController.h
@@ -0,0 +1,18 @@
+//
+//  CZSearchAllViewController.h
+//  CZOrganizerSDK
+//
+//  Created by 赵曙诚 on 2020/4/8.
+//  Copyright © 2020 zsc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CZScrollContentControllerDeleagte.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CZSearchAllViewController : UIViewController<CZScrollContentControllerDeleagte>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CZOrganizerSDK/CZOrganizerSDK/UI/Search/CZSearchAllViewController.m
@@ -0,0 +1,92 @@
+//
+//  CZSearchAllViewController.m
+//  CZOrganizerSDK
+//
+//  Created by 赵曙诚 on 2020/4/8.
+//  Copyright © 2020 zsc. All rights reserved.
+//
+
+#import "CZSearchAllViewController.h"
+#import "CZPageScrollContentView.h"
+#import "CZCarefullyChooseViewController.h"
+#import "CZDiaryViewController.h"
+#import "CZSchoolStarViewController.h"
+#import "CZAdvisorViewController.h"
+#import "CZOrganizerListViewController.h"
+#import "SPPageMenu.h"
+#import "QSClient.h"
+
+@interface CZSearchAllViewController ()<SPPageMenuDelegate>
+@property (nonatomic , strong)CZPageScrollContentView *contentView;
+@property (nonatomic , strong)SPPageMenu *pageMenu;
+@end
+
+@implementation CZSearchAllViewController
+@synthesize contentScrollView;
+@synthesize canScroll;
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self initUI];
+}
+
+-(void)initUI
+{
+    NSArray *vcs = [self contentScrollers];
+    
+    self.pageMenu = [SPPageMenu pageMenuWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 50) trackerStyle:SPPageMenuTrackerStyleLine];
+    self.pageMenu.permutationWay = SPPageMenuPermutationWayNotScrollAdaptContent;
+    [self.pageMenu setItems:@[NSLocalizedString(@"精选", nil),NSLocalizedString(@"口碑", nil),NSLocalizedString(@"达人", nil),NSLocalizedString(@"顾问", nil),NSLocalizedString(@"机构", nil)] selectedItemIndex:0];
+    self.pageMenu.itemTitleFont = [UIFont boldSystemFontOfSize:16];
+    self.pageMenu.tracker.backgroundColor = self.pageMenu.unSelectedItemTitleColor = CZColorCreater(51, 172, 253, 1);
+    self.pageMenu.delegate = self;
+    self.pageMenu.selectedItemTitleColor = [UIColor blackColor];
+    self.pageMenu.unSelectedItemTitleColor = CZColorCreater(167, 167, 185, 1);
+    [self.view addSubview:self.pageMenu];
+    
+    self.contentView = [[CZPageScrollContentView alloc]initWithFrame:CGRectMake(0, self.pageMenu.bounds.size.height, self.view.bounds.size.width, self.view.bounds.size.height-self.pageMenu.bounds.size.height) contentControllers:vcs rootController:self];
+    [self.view addSubview:self.contentView];
+    self.pageMenu.bridgeScrollView = self.contentView.collectionView;
+}
+
+-(void)actionForShowCart
+{
+    [QSClient showCartInNavi:self.navigationController];
+}
+
+-(void)actionForSearch
+{
+    
+}
+
+
+
+// 子类化实现
+- (NSArray<UIViewController<CZScrollContentControllerDeleagte> *> *)contentScrollers
+{
+    NSMutableArray *arry = [[NSMutableArray alloc]init];
+    CZCarefullyChooseViewController *controller1 = [[CZCarefullyChooseViewController alloc]init];
+    [arry addObject:controller1];
+    CZDiaryViewController *controller2 = [[CZDiaryViewController alloc]init];
+    [arry addObject:controller2];
+    CZSchoolStarViewController *controller3 = [[CZSchoolStarViewController alloc]init];
+    [arry addObject:controller3];
+    CZAdvisorViewController *controller4 = [[CZAdvisorViewController alloc]init];
+    [arry addObject:controller4];
+    CZOrganizerListViewController *controller5 = [[CZOrganizerListViewController alloc]init];
+    [arry addObject:controller5];
+
+    return arry;
+}
+
+-(void)actionForBack
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)pageMenu:(SPPageMenu *)pageMenu itemSelectedFromIndex:(NSInteger)fromIndex toIndex:(NSInteger)toIndex
+{
+    [self.contentView.collectionView setContentOffset:CGPointMake(toIndex*self.view.bounds.size.width, 0) animated:YES];
+}
+
+@end
